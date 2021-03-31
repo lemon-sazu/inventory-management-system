9606072211a71c7576b67feea8585f2e1311dca1
lemon-sazu <lemon.sazu@gmail.com>
2021-03-31 12:22:56 +0600

    Initial setup

---
+++ resources/views/layouts/partials/_navbar.blade.php
@@ -0,0 +1,25 @@
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      
+    </ul>
+
+    <!-- SEARCH FORM -->
+  
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+      <!-- Messages Dropdown Menu -->
+   
+      <!-- Notifications Dropdown Menu -->
+     
+      <li class="nav-item">
+        <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+          <i class="fas fa-expand-arrows-alt"></i>
+        </a>
+      </li>
+ 
+    </ul>
+  </nav>
+  <!-- /.navbar -->
